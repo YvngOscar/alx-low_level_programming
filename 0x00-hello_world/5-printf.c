342a06ffd392f62ec480a396de5c93e32d50e125
Oscar Kiarie <oscarmwas65@gmail.com>
2023-03-18 05:05:07 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - always main
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
