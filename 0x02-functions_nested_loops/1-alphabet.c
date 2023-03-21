e77f6381a055e6df67747de4dd4dab02a7172938
Oscar Kiarie <oscarmwas65@gmail.com>
2023-03-21 04:34:44 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *print_alphabet - this is the main function
+ * print_alphabet - this is the main function
  *
  * Return: always 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
