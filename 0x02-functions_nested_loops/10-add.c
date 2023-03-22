859952e0f396e6133128e0786b4e7dcd5b798a02
Oscar Kiarie <oscarmwas65@gmail.com>
2023-03-22 02:53:42 -0700

    adds integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - this is the mian function
+ * @a: this is a parameter
+ * @b: this is also a parameter
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
