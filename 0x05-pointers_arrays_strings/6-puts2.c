41213e9d38ac9732a99a4dd1c101d537ffc633cf
Oscar Kiarie <oscarmwas65@gmail.com>
2023-03-28 11:02:38 -0700

    every other number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: the string to be printed
+ * puts2 - prints xters of a string
+ * @str: the used string reference
+ * Return: 0
  */
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
 	}
 	_putchar('\n');
