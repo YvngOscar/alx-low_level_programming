11c7f18a859eb34ad847c76eced749ea8cb5a548
Oscar Kiarie <oscarmwas65@gmail.com>
2023-03-30 11:48:55 -0700

    copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int strlen = 0;
 
+	for (i = 0 ; src[i] != '\0' ; i++)
+		strlen++;
 	for (i = 0 ; src[i] && i < n ; i++)
 		dest[i] = src[i];
+	for (i = strlen ; i < n ; i++)
+		dest[i] = '\0';
 	return (dest);
 }
