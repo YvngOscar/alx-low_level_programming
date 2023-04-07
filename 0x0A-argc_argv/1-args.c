33a21c5f2b61c1131becb99a533b16e716bd3a43
Oscar Kiarie <oscarmwas65@gmail.com>
2023-04-07 10:12:38 -0700

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0 for success
+ * @argc: the parameter
+ * @argv: parameter
+ *
+ *
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
