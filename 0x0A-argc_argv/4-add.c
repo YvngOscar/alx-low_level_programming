d62e403d259b25cca0d4422ed909e69f59f2a8e3
Oscar Kiarie <oscarmwas65@gmail.com>
2023-04-07 12:49:18 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
- * main - the main function
- * @argc:parameter
- * @argv:parameter
- * Return: 0 for success
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	int i, j, add = 0;
 
-	for (i = 0 ; i < argc ; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0 ; argv[i][j] != '\0' ; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
